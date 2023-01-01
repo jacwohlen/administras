ef4f7e439f3c0f7ff56cc492d1fd93cc7b9dff26
Andreas Schmid <andreas.schmid.as3@roche.com>
2023-01-02 00:42:55 +0100

    Baseline: Working Authentication and Member Display

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Button, Row, Col } from 'sveltestrap';
+	import authStore from '$lib/authStore';
+	import { goto } from '$app/navigation';
+
+	$: if (!$authStore) {
+		goto('/');
+	}
+</script>
+
+Dashboard - Layout
+<Row>
+	<Col>
+		<Button href="/dashboard" color="primary">Today</Button>
+	</Col>
+	<Col>
+		<Button href="/dashboard/trainings" color="primary">Trainings</Button>
+	</Col>
+	<Col>
+		<Button href="/dashboard/members" color="primary">Members</Button>
+	</Col>
+	<Col>
+		<Button on:click={authStore.signOut}>Sign Out</Button>
+	</Col>
+</Row>
+
+<slot />
