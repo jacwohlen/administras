e09e374a2f41516f909a041a18647ef70a7bef71
Andreas Schmid <ikeark@gmail.com>
2023-01-02 00:53:01 +0100

    Code Format: Use spaces instead of tabs

--- src/lib/authStore.ts
+++ src/lib/authStore.ts
@@ -4,23 +4,23 @@ import type { User } from 'firebase/auth';
 import { PUBLIC_GOOGLE_LOGIN_DOMAIN } from '$env/static/public';
 
 const authStore = readable<User | null>(undefined, (set) => {
-	const unsubsribe = auth.onAuthStateChanged((user) => set(user));
-	return unsubsribe;
+  const unsubsribe = auth.onAuthStateChanged((user) => set(user));
+  return unsubsribe;
 });
 
 async function signIn() {
-	const { signInWithRedirect, GoogleAuthProvider } = await import('firebase/auth');
-	const provider = new GoogleAuthProvider();
-	provider.setCustomParameters({ hd: PUBLIC_GOOGLE_LOGIN_DOMAIN });
-	await signInWithRedirect(auth, provider);
+  const { signInWithRedirect, GoogleAuthProvider } = await import('firebase/auth');
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({ hd: PUBLIC_GOOGLE_LOGIN_DOMAIN });
+  await signInWithRedirect(auth, provider);
 }
 
 async function signOut() {
-	await auth.signOut();
+  await auth.signOut();
 }
 
 export default {
-	subscribe: authStore.subscribe,
-	signIn: signIn,
-	signOut: signOut
+  subscribe: authStore.subscribe,
+  signIn: signIn,
+  signOut: signOut
 };
--- src/lib/models.ts
+++ src/lib/models.ts
@@ -5,29 +5,29 @@ export enum WEEKDAY {
   Wednesday = 3,
   Thursday = 4,
   Friday = 5,
-  Saturday = 6,
+  Saturday = 6
 }
 
 export interface Member {
-  id: string
-  firstname: string
-  lastname: string
-  birthday?: string
-  mobile?: string
-  labels?: string[]
+  id: string;
+  firstname: string;
+  lastname: string;
+  birthday?: string;
+  mobile?: string;
+  labels?: string[];
 }
 
 export interface Training {
-  id: string
-  title: string
-  dateFrom: string
-  dateTo: string
-  weekday: string
-  section: string
-  participants: Member[]
+  id: string;
+  title: string;
+  dateFrom: string;
+  dateTo: string;
+  weekday: string;
+  section: string;
+  participants: Member[];
 }
 
 export interface Log {
-  id: string // date
-  members: Member[]
+  id: string; // date
+  members: Member[];
 }
