b9bec7d4e956736975a11ed5967802cd571917fb
Andreas Schmid <andreas.schmid.as3@roche.com>
2023-01-07 01:25:15 +0100

    Workaround: Sveltekit does not allow to fetch from parent page yet
    As a workaround moved data fetching logic into +layout.ts instead of +page.ts.
    This way the parent() fetching in the child page works

--- src/routes/dashboard/trainings/[trainingId]/+page.ts
+++ src/routes/dashboard/trainings/[trainingId]/+page.ts
@@ -1,16 +1,5 @@
-import { error } from '@sveltejs/kit';
-import { db } from '$lib/firebase';
-import type { Training } from '$lib/models';
-import { getDoc, doc } from 'firebase/firestore';
-
-export async function load({ params }) {
-  const docRef = doc(db, `trainings/${params.trainingId}`);
-  const docSnap = await getDoc(docRef);
-  if (!docSnap.exists()) {
-    throw error(404, 'Not found');
-  }
-
-  return {
-    ...(docSnap.data() as Training)
-  };
+export async function load({ parent }) {
+  const parentData = await parent();
+  // FIXME: Could load data here directly and not put it into layout.ts if sveltekit would allow
+  return parentData;
 }
