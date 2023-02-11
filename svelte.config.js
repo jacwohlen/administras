3d982dcdfcd5e4570a7f1de8f3acd783c26680ce
Andreas Schmid <andreas.schmid.as3@roche.com>
2023-02-11 23:01:08 +0100

    Remove attractions UI

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-netlify';
-import sveltePreprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter()
