f05f2cde21ff920a13ab07740b523c0f2dc9136c
Lukas Schmid <dimschlukas@gmail.com>
2023-02-12 16:19:02 +0100

    Replace appbar buttons with tabs

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,27 +1,45 @@
 <script lang="ts">
-  import { Button, Row, Col } from 'sveltestrap';
   import authStore from '$lib/authStore';
   import { goto } from '$app/navigation';
+  import { AppBar } from '@skeletonlabs/skeleton';
+  import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+  import { page } from '$app/stores';
 
   $: if ($authStore.firebaseControlled && !$authStore.user) {
     goto('/');
   }
+
+  let tabSet: number = 0;
+  if ($page.route.id == '/dashboard') {
+    tabSet = 0;
+  } else if ($page.route.id?.startsWith('/dashboard/trainings')) {
+    tabSet = 1;
+  } else if ($page.route.id?.startsWith('/dashboard/members')) {
+    tabSet = 2;
+  } else if ($page.route.id?.startsWith('/dashboard/statistics')) {
+    tabSet = 3;
+  }
 </script>
 
 {#if $authStore.firebaseControlled && $authStore.user}
-  <Row>
-    <Col>
-      <Button href="/dashboard" color="primary">Today</Button>
-    </Col>
-    <Col>
-      <Button href="/dashboard/trainings" color="primary">Trainings</Button>
-    </Col>
-    <Col>
-      <Button href="/dashboard/members" color="primary">Members</Button>
-    </Col>
-    <Col>
-      <Button on:click={authStore.signOut}>Sign Out</Button>
-    </Col>
-  </Row>
+  <TabGroup>
+    <Tab bind:group={tabSet} name="tab1" value={0} on:click={() => goto('/dashboard')}>
+      <i class="fa-regular fa-calendar-check" />
+      <div class="hidden sm:block">Today</div>
+    </Tab>
+    <Tab bind:group={tabSet} name="tab2" value={1} on:click={() => goto('/dashboard/trainings')}>
+      <i class="fa-solid fa-list" />
+      <div class="hidden sm:block">Trainings</div>
+    </Tab>
+    <Tab bind:group={tabSet} name="tab3" value={2} on:click={() => goto('/dashboard/members')}>
+      <i class="fa-regular fa-user" />
+      <div class="hidden sm:block">Members</div>
+    </Tab>
+    <button class="btn" on:click={authStore.signOut}>
+      <i class="fa-solid fa-right-from-bracket" />
+      <div class="hidden sm:block">Sign Out</div>
+    </button>
+  </TabGroup>
+
   <slot />
 {/if}
