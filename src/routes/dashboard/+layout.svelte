31eacfcb5857dd57d79c7f6634b03fb9d7dbba41
Lukas Schmid <dimschlukas@gmail.com>
2023-02-12 17:12:11 +0100

    Add photoUrl profile button with logout button in menu

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import authStore from '$lib/authStore';
   import { goto } from '$app/navigation';
-  import { AppBar } from '@skeletonlabs/skeleton';
   import { TabGroup, Tab } from '@skeletonlabs/skeleton';
   import { page } from '$app/stores';
+  import { Avatar, menu, AppShell, AppBar } from '@skeletonlabs/skeleton';
+  import { auth } from '$lib/firebase';
 
   $: if ($authStore.firebaseControlled && !$authStore.user) {
     goto('/');
@@ -19,27 +20,66 @@
   } else if ($page.route.id?.startsWith('/dashboard/statistics')) {
     tabSet = 3;
   }
+
+  function getInitials(): string {
+    if ($authStore.user?.displayName == null) {
+      return '';
+    }
+    const arr = $authStore.user?.displayName?.split(' ');
+    if (arr?.length >= 2) {
+      return arr[0].charAt(0) + arr[1].charAt(0);
+    } else {
+      return arr[0].charAt(0);
+    }
+  }
 </script>
 
 {#if $authStore.firebaseControlled && $authStore.user}
-  <TabGroup>
-    <Tab bind:group={tabSet} name="tab1" value={0} on:click={() => goto('/dashboard')}>
-      <i class="fa-regular fa-calendar-check" />
-      <div class="hidden sm:block">Today</div>
-    </Tab>
-    <Tab bind:group={tabSet} name="tab2" value={1} on:click={() => goto('/dashboard/trainings')}>
-      <i class="fa-solid fa-list" />
-      <div class="hidden sm:block">Trainings</div>
-    </Tab>
-    <Tab bind:group={tabSet} name="tab3" value={2} on:click={() => goto('/dashboard/members')}>
-      <i class="fa-regular fa-user" />
-      <div class="hidden sm:block">Members</div>
-    </Tab>
-    <button class="btn" on:click={authStore.signOut}>
-      <i class="fa-solid fa-right-from-bracket" />
-      <div class="hidden sm:block">Sign Out</div>
-    </button>
-  </TabGroup>
+  <AppShell>
+    <svelte:fragment slot="header">
+      <TabGroup>
+        <Tab bind:group={tabSet} name="tab1" value={0} on:click={() => goto('/dashboard')}>
+          <i class="fa-regular fa-calendar-check" />
+          <div>Today</div>
+        </Tab>
+        <Tab
+          bind:group={tabSet}
+          name="tab2"
+          value={1}
+          on:click={() => goto('/dashboard/trainings')}
+        >
+          <i class="fa-solid fa-list" />
+          <div>Trainings</div>
+        </Tab>
+        <Tab bind:group={tabSet} name="tab3" value={2} on:click={() => goto('/dashboard/members')}>
+          <i class="fa-regular fa-user" />
+          <div>Members</div>
+        </Tab>
 
-  <slot />
+        <div class="ml-auto my-auto pr-4" use:menu={{ menu: 'example' }}>
+          {#if auth.currentUser?.photoURL != null}
+            <Avatar
+              src={auth.currentUser?.photoURL}
+              border="border-4 border-surface-300-600-token hover:!border-primary-500"
+              cursor="cursor-pointer"
+            />
+          {:else}
+            <Avatar
+              initials={getInitials()}
+              border="border-4 border-surface-300-600-token hover:!border-primary-500"
+              cursor="cursor-pointer"
+            />
+          {/if}
+        </div>
+        <nav class="list-nav card p-4 w-64 shadow-xl" data-menu="example">
+          <ul>
+            <li>
+              <button class="option w-full" on:click={authStore.signOut}> Logout </button>
+            </li>
+          </ul>
+        </nav>
+      </TabGroup>
+    </svelte:fragment>
+    <slot />
+  </AppShell>
 {/if}
