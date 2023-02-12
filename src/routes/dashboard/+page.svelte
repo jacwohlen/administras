c5b4e19c4128569f17df5e0ba030f7d986013542
Lukas Schmid <dimschlukas@gmail.com>
2023-02-12 14:51:26 +0100

    switch to skeleton from sveltestrap

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,29 +1,25 @@
 <script lang="ts">
   import type { PageData } from './$types';
-  import { Row, Col, ListGroup, ListGroupItem, Button, Icon, Alert } from 'sveltestrap';
   export let data: PageData;
 </script>
 
 <h1>Today</h1>
 {#if data.trainings.length == 0}
-  <Alert color="info" fade={false}>There are no trainings happening today.</Alert>
+  <div class="text-center">There are no trainings happening today!</div>
 {:else}
-  <ListGroup>
+  <ul class="list">
     {#each data.trainings as t (t.id)}
-      <ListGroupItem>
-        <Row>
-          <Col>
-            {t.id}
-            {t.title}
-          </Col>
-          <Col>
-            <Button size="sm" href="/dashboard/trainings/{t.id}">
-              <Icon name="text-paragraph" />
-              View
-            </Button>
-          </Col>
-        </Row>
-      </ListGroupItem>
+      <li>
+        <span class="flex-auto">
+          {t.title}
+        </span>
+        <span class="">
+          <a class="btn btn-sm variant-filled-primary" href="/dashboard/trainings/{t.id}">
+            <i class="fa-solid fa-clipboard-check mr-2" />
+            Track attandence
+          </a>
+        </span>
+      </li>
     {/each}
-  </ListGroup>
+  </ul>
 {/if}
