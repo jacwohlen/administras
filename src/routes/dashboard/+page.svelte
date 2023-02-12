5cd952b89b991801f015e7badf449f0da9b166cb
Lukas Schmid <dimschlukas@gmail.com>
2023-02-12 13:36:17 +0100

    list current trainings on today page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Internal</h1>
+<script lang="ts">
+  import type { PageData } from './$types';
+  import { Row, Col, ListGroup, ListGroupItem, Button, Icon, Alert } from 'sveltestrap';
+  export let data: PageData;
+</script>
+
+<h1>Today</h1>
+{#if data.trainings.length == 0}
+  <Alert color="info" fade={false}>There are no trainings happening today.</Alert>
+{:else}
+  <ListGroup>
+    {#each data.trainings as t (t.id)}
+      <ListGroupItem>
+        <Row>
+          <Col>
+            {t.id}
+            {t.title}
+          </Col>
+          <Col>
+            <Button size="sm" href="/dashboard/trainings/{t.id}">
+              <Icon name="text-paragraph" />
+              View
+            </Button>
+          </Col>
+        </Row>
+      </ListGroupItem>
+    {/each}
+  </ListGroup>
+{/if}
