af2a3b836fa9d97a615f334e400089282cbc72a1
Lukas Schmid <dimschlukas@gmail.com>
2023-02-14 20:46:10 +0100

    Add button group to choose date

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,17 +1,58 @@
 <script lang="ts">
-  import type { PageData } from './$types';
-  import Fa from 'svelte-fa'
-  import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
+  import Fa from 'svelte-fa';
+  import { faClipboardCheck, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+  import { db } from '$lib/firebase';
+  import { collection, getDocs, query, where } from 'firebase/firestore';
+  import type { Training } from '$lib/models';
+  import moment from 'moment';
+  import type { Moment } from 'moment';
 
-  export let data: PageData;
+  let currentDate: Moment = moment();
+  let trainings: Training[] = [];
+
+  function nextDay() {
+    currentDate = currentDate.add(1, 'days');
+    getTrainingsForDay();
+  }
+
+  function previousDay() {
+    currentDate = currentDate.subtract(1, 'days');
+    getTrainingsForDay();
+  }
+
+  async function getTrainingsForDay() {
+    console.log('getting data for date: ' + currentDate.format('YYYY-MM-DD'));
+    const ret: Training[] = [];
+    const q = query(
+      collection(db, 'trainings'),
+      where('weekday', '==', currentDate.format('dddd'))
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      ret.push({ ...(doc.data() as Training), id: doc.id });
+    });
+    console.log(ret);
+    trainings = ret;
+  }
+  getTrainingsForDay();
 </script>
 
 <h1>Today</h1>
-{#if data.trainings.length == 0}
+<div class="text-center p-4 .w-4/5">
+  <div class="btn-group variant-filled-primary">
+    <button on:click={previousDay}><Fa icon={faArrowLeft} /> </button>
+    <button class="w-64">
+      {currentDate.format('dddd DD.MM.YYYY')}
+    </button>
+    <button on:click={nextDay}><Fa icon={faArrowRight} /> </button>
+  </div>
+</div>
+
+{#if trainings.length == 0}
   <div class="text-center">There are no trainings happening today!</div>
 {:else}
   <ul class="list">
-    {#each data.trainings as t (t.id)}
+    {#each trainings as t (t.id)}
       <li>
         <span class="flex-auto">
           {t.title}
