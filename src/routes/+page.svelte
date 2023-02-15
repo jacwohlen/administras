7a48213ac7fe5de5a7c8e107ea0e42ab7c8a4934
Andreas Schmid <andreas.schmid.as3@roche.com>
2023-02-16 00:30:16 +0100

    Add administra logo to start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
   import authStore from '$lib/authStore';
+  import LogoImage from './LogoImage.svelte';
 </script>
 
-<h1>Welcome to Administra</h1>
-{#if $authStore.firebaseControlled && $authStore.user}
-  <p>
-    Hi {$authStore.user.email}
-  </p>
-  <a class="btn variant-filled-primary" href="/dashboard" color="primary">Open Dashboard</a>
-{:else}
-  <button class="btn variant-filled-primary" on:click={authStore.signIn} color="primary">
-    Login
-  </button>
-{/if}
+<div class="grid h-screen place-items-center">
+  <div>
+    <LogoImage /> 
+  </div>
+  <div>
+    <h1>Welcome to Administra</h1>
+    {#if $authStore.firebaseControlled && $authStore.user}
+      <p>
+        Hi {$authStore.user.email}
+      </p>
+      <a class="btn variant-filled-primary" href="/dashboard" color="primary">Open Dashboard</a>
+    {:else}
+      <button class="btn variant-filled-primary" on:click={authStore.signIn} color="primary">
+        Login
+      </button>
+    {/if}
+  </div>
+</div>
---
+++ src/routes/LogoImage.svelte
@@ -0,0 +1,16 @@
+<img
+  class="rotating"
+  src="/administra-logo.svg"
+  alt="administra logo"
+/>
+
+<style>
+.rotating {
+  animation: rotate 2s;
+}
+@keyframes rotate {
+  to {
+    transform: rotate(-360deg);
+  }
+}
+</style>
