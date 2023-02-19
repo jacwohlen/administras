9a4c55e5c7709d0c5859238255512eb011c4e767
Lukas Schmid <dimschlukas@gmail.com>
2023-02-19 11:56:37 +0100

    Theme: implement custom theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
   import '@skeletonlabs/skeleton/themes/theme-sahara.css';
+  import '../theme.postcss';
   import '../app.postcss';
-  import '@skeletonlabs/skeleton/styles/all.css'; 
+  import '@skeletonlabs/skeleton/styles/all.css';
 </script>
 
 <slot />
