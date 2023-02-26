20e7d2b38e44d9a0fc7abe9392481ae96c4b9b43
Andreas Schmid <ikeark@gmail.com>
2023-02-26 20:31:00 +0100

    Supabase: Protect dashboard/*

--- src/routes/dashboard/+layout.ts
+++ src/routes/dashboard/+layout.ts
@@ -1,2 +1,12 @@
 // FIXME: Implement SSR with Firebase
 export const ssr = false;
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async (event) => {
+	const { session } = await getSupabase(event);
+	if (!session) {
+		throw redirect(303, '/');
+	}
+};
