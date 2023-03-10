87dd93666b831b9a773084be97e410ff1ccf1269
Lukas Schmid <dimschlukas@gmail.com>
2023-03-10 22:39:01 +0100

    Setting up i18n translation

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,16 @@
 import type { LayoutLoad } from './$types';
+import { browser } from '$app/environment'
+import '$lib/i18n'
+import { locale, waitLocale } from 'svelte-i18n'
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
 	const { session } = await getSupabase(event);
+
+	if (browser) {
+		locale.set(window.navigator.language)
+	}
+	await waitLocale()
+
 	return { session };
 };
