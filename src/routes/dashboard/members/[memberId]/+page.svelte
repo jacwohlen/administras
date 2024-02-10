9de809340f5cfb8717a5a5588b4d2483ed0c5f72
Lukas Schmid <dimschlukas@gmail.com>
2024-02-10 20:24:27 +0100

    Member view: Add pagination and per-year-view

--- src/routes/dashboard/members/[memberId]/+page.svelte
+++ src/routes/dashboard/members/[memberId]/+page.svelte
@@ -6,47 +6,52 @@
   export let data: PageData;
 </script>
 
-<div class="card p-4">
-  <div class="mb-4">
-    {#if data.img}
-      <Avatar class="mx-auto w-56" src={data.img} />
-    {:else}
-      <Avatar class="mx-auto w-56" initials={data.lastname.charAt(0) + data.firstname.charAt(0)} />
-    {/if}
+<div class="space-y-4">
+  <div class="card p-4">
+    <div class="mb-4">
+      {#if data.img}
+        <Avatar class="mx-auto w-56" src={data.img} />
+      {:else}
+        <Avatar
+          class="mx-auto w-56"
+          initials={data.lastname.charAt(0) + data.firstname.charAt(0)}
+        />
+      {/if}
+    </div>
+    <div class="table-container">
+      <table class="table">
+        <tr>
+          <td><b>{$_('page.members.id')}</b></td>
+          <td>{data.id}</td>
+        </tr>
+        <tr>
+          <td><b>{$_('page.members.lastName')}</b></td>
+          <td>{data.lastname}</td>
+        </tr>
+        <tr>
+          <td><b>{$_('page.members.firstName')}</b></td>
+          <td>{data.firstname}</td>
+        </tr>
+        <tr>
+          <td><b>{$_('page.members.birthday')}</b></td>
+          <td>{data.birthday}</td>
+        </tr>
+        <tr>
+          <td><b>{$_('page.members.mobile')}</b></td>
+          <td>{data.mobile}</td>
+        </tr>
+        <tr>
+          <td><b>{$_('page.members.labels')}</b></td>
+          <td>
+            {#if data.labels}
+              {#each data.labels as l}
+                <span class="chip mr-2">{l}</span>
+              {/each}
+            {/if}
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
-  <div class="table-container">
-    <table class="table">
-      <tr>
-        <td><b>{$_('page.members.id')}</b></td>
-        <td>{data.id}</td>
-      </tr>
-      <tr>
-        <td><b>{$_('page.members.lastName')}</b></td>
-        <td>{data.lastname}</td>
-      </tr>
-      <tr>
-        <td><b>{$_('page.members.firstName')}</b></td>
-        <td>{data.firstname}</td>
-      </tr>
-      <tr>
-        <td><b>{$_('page.members.birthday')}</b></td>
-        <td>{data.birthday}</td>
-      </tr>
-      <tr>
-        <td><b>{$_('page.members.mobile')}</b></td>
-        <td>{data.mobile}</td>
-      </tr>
-      <tr>
-        <td><b>{$_('page.members.labels')}</b></td>
-        <td>
-          {#if data.labels}
-            {#each data.labels as l}
-              <span class="chip mr-2">{l}</span>
-            {/each}
-          {/if}
-        </td>
-      </tr>
-    </table>
-  </div>
+  <MemberLogList memberId={data.id} />
 </div>
-<MemberLogList memberId={data.id} />
--- src/routes/dashboard/members/[memberId]/MemberLogList.svelte
+++ src/routes/dashboard/members/[memberId]/MemberLogList.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-  import type { Log } from '$lib/models';
   import { error as err } from '@sveltejs/kit';
   import Fa from 'svelte-fa';
+  import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
   import { faGripLines } from '@fortawesome/free-solid-svg-icons';
   import { supabaseClient } from '$lib/supabase';
   import { _ } from 'svelte-i18n';
 
   export let memberId: String;
+
+  $: year = new Date().getFullYear();
 
   interface LogSummary {
     trainingId: number;
@@ -26,6 +28,8 @@
       .from('logs')
       .select()
       .eq('memberId', memberId)
+      .gte('date', year + '-01-01')
+      .lte('date', year + '-12-31')
       .order('date', { ascending: false })
       .returns<LogSummary[]>();
 
@@ -56,31 +60,77 @@
     }));
     return enrichedLogs;
   }
+
+  async function previousYear() {
+    year = year - 1;
+    currentItem = 10;
+    l = getLogs();
+  }
+
+  async function nextYear() {
+    year = year + 1;
+    currentItem = 10;
+    l = getLogs();
+  }
+  let currentItem = 10;
+
+  $: l = getLogs();
 </script>
 
-{#await getLogs() then logs}
-  <ul class="list">
-    {#each logs as i}
-      <li>
-        <span>
-          {i.date}
+<div class="card p-4">
+  <h3>{$_('page.members.trainingsHistory.title')}</h3>
+  <div class="flex justify-between items-center m-2">
+    <div>
+      <button class="btn" on:click={previousYear}>
+        <Fa icon={faArrowLeft} /><span>{$_('button.year')}</span>
+      </button>
+    </div>
+    <div>
+      {year}
+    </div>
+    <div>
+      <button class="btn" on:click={nextYear}>
+        <span>{$_('button.year')}</span><Fa icon={faArrowRight} />
+      </button>
+    </div>
+  </div>
+
+  {#await l then logs}
+    <ul class="list">
+      {#each logs.slice(0, currentItem) as i}
+        <li>
+          <span>
+            {i.date}
+          </span>
+          <span class="flex-auto">
+            {i.trainingsInfo.title}
+          </span>
+          <span class="flex-right">
+            {i.trainingsInfo.section}
+          </span>
+          <span>
+            <a
+              class="btn btn-sm variant-filled-secondary"
+              href="/dashboard/trainings/{i.trainingId}/{i.date}"
+            >
+              <Fa icon={faGripLines} />
+              <span>{$_('button.view')}</span>
+            </a>
+          </span>
+        </li>
+      {:else}
+        <span class="flex justify-center">
+          {$_('page.members.trainingsHistory.noItems')}
         </span>
-        <span class="flex-auto">
-          {i.trainingsInfo.title}
-        </span>
-        <span class="flex-right">
-          {i.trainingsInfo.section}
-        </span>
-        <span>
-          <a
-            class="btn btn-sm variant-filled-secondary"
-            href="/dashboard/trainings/{i.trainingId}/{i.date}"
-          >
-            <Fa icon={faGripLines} />
-            <span>{$_('button.view')}</span>
-          </a>
-        </span>
-      </li>
-    {/each}
-  </ul>
-{/await}
+      {/each}
+    </ul>
+    {#if currentItem < logs.length}
+      <button
+        class="btn btn-sm variant-filled-secondary"
+        on:click={() => (currentItem = currentItem + 10)}
+      >
+        {$_('button.loadMore')}
+      </button>
+    {/if}
+  {/await}
+</div>
