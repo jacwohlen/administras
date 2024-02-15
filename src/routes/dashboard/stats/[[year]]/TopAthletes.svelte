59043bfb87bcbc58c867301f268880b64659f5e4
Lukas Schmid <dimschlukas@gmail.com>
2024-02-15 22:42:14 +0100

    Stats: Fix wrong padding on top athletes list view on safari browser

--- src/routes/dashboard/stats/[[year]]/TopAthletes.svelte
+++ src/routes/dashboard/stats/[[year]]/TopAthletes.svelte
@@ -12,9 +12,9 @@
 <div class="flex flex-row overflow-x-auto snap-x gap-4">
   {#each Object.keys(topAthletes) as section}
     <div class="flex-none snap-start pl-4 pt-2 pb-4 card bg-white">
-      <h3 class="indent-4">{section}</h3>
+      <h3 class="indent-2">{section}</h3>
       <div class="overflow-y-auto h-60 w-60">
-        <ol class="list-decimal list-outside pl-6">
+        <ol class="list-decimal list-outside pl-8">
           {#each topAthletes[section] as item}
             <li>
               <span class="text-nowrap">{item.lastname} {item.firstname} ({item.count})</span>
