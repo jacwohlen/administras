b1e45a5685e71a7d8c38f8854665619a83417c49
Lukas Schmid <dimschlukas@gmail.com>
2024-03-27 22:12:39 +0100

    MembersView: Add funcitonality to upload profile pictures

--- src/routes/dashboard/members/[memberId]/+page.svelte
+++ src/routes/dashboard/members/[memberId]/+page.svelte
@@ -3,7 +3,55 @@
   import { Avatar } from '@skeletonlabs/skeleton';
   import { _ } from 'svelte-i18n';
   import MemberLogs from './MemberLogs.svelte';
+  import { faCamera, faTrash } from '@fortawesome/free-solid-svg-icons';
+  import { supabaseClient } from '$lib/supabase';
+  import { error as err } from '@sveltejs/kit';
+  import Fa from 'svelte-fa';
+
   export let data: PageData;
+
+  function handlePhotoChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file: File = input.files[0];
+      data.img = URL.createObjectURL(file);
+      updateSupabaseMember(file);
+    }
+  }
+
+  function updateSupabaseMember(file: File) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async function () {
+      const { data: member, error } = await supabaseClient
+        .from('members')
+        .update({ img: reader.result })
+        .eq('id', data.id)
+        .select();
+      if (error) {
+        throw err(404, error);
+      }
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+  }
+
+  async function resetImage() {
+    const { data: member, error } = await supabaseClient
+      .from('members')
+      .update({ img: null })
+      .eq('id', data.id)
+      .select();
+    if (error) {
+      throw err(404, error);
+    }
+    data.img = undefined;
+  }
+
+  function selectFiles() {
+    document.getElementById('selectedFile')?.click();
+  }
 </script>
 
 <div class="space-y-4">
@@ -17,6 +65,22 @@
           initials={data.lastname.charAt(0) + data.firstname.charAt(0)}
         />
       {/if}
+      <div class="flex flex-row justify-center">
+        <input
+          type="file"
+          id="selectedFile"
+          style="display: none;"
+          accept="image/*"
+          on:change={handlePhotoChange}
+        />
+        <button class="btn variant-filled-secondary" on:click={selectFiles}
+          ><Fa icon={faCamera} /></button
+        >
+        <div class="w-20" />
+        <button class="btn variant-filled-secondary" on:click={resetImage}
+          ><Fa icon={faTrash} /></button
+        >
+      </div>
     </div>
     <div class="table-container">
       <table class="table">
